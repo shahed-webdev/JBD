5b1867fcdc5bfc4151c94e0ba3b8f4c048b3e751
Shahed <Shahedloopsit@gmail.com>
2023-03-23 16:59:00 +0600

    CSV file upload

--- JBD.WEB/Controllers/SellerController.cs
+++ JBD.WEB/Controllers/SellerController.cs
@@ -13,6 +13,26 @@ namespace JBD.WEB.Controllers
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public IActionResult CsvDataUpload(IFormFile? file)
+        {
+            if (file == null) return BadRequest();
+
+            var data = new List<string>();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    var line = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                        data.Add(line);
+
+                    // do something with the line of data
+                }
+            }
+
+            return Ok(data);
+        }
     }
 }
---
+++ JBD.WEB/wwwroot/assets/js/custom/themeCSVuploaded.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+// Class definition
+var myDropzone = new Dropzone("#kt_dropzonejs_example_1", {
+    url: "/Seller/CsvDataUpload", // Set the url for your upload script location
+    paramName: "file", // The name that will be used to transfer the file
+    maxFiles: 10,
+    maxFilesize: 10, // MB
+    addRemoveLinks: true,
+    accept: function (file, done) {
+        if (file.name == "wow.jpg") {
+            done("Naha, you don't.");
+        } else {
+            done();
+        }
+    }
+});
